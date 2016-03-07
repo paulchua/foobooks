bf75acc544a1ec6a96887fd99bceb48582afe71c
Paul Chua <paulchua@g.harvar.edu>
2016-03-06 19:14:36 -0800

    Demonstrating environment configs

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,8 +11,28 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+
+Route::group(['middleware' => ['web']], function () {
+
+	Route::get('/', function () {
+		//return view('welcome');
+		return 'Hello welcome to my Laravel application!';
+	});
+	
+	Route::get('/books', 'BookController@getIndex');
+	Route::get('/book/create', 'BookController@getCreate');		
+	Route::post('/book/create/', 'BookController@postCreate');
+	Route::get('/book/{id}', 'BookController@getShow');
+	
+	Route::get('/practice', function() {
+		
+		echo 'app.url: '.config('app.url');
+		echo '<br>app.env: '.config('app.env');
+		
+		return '';
+		
+	});
 });
 
 /*
